74ba68422982fb19b448fbcb373874d37bfd5c37
Luka Souza <lukasouza77@gmail.com>
2023-06-27 02:57:26 -0300

    header & home done

--- src/modules/js/Header.js
+++ src/modules/js/Header.js
@@ -1,33 +1,23 @@
 import '../css/header.css';
 import Typing from './scripts/typing.js';
-import { useRef, useEffect } from "react";
+import { useRef } from "react";
 
 
 export default function Header() {
-
     const mobile = useRef();
-    const items = useRef([]);
-    const pushItems = (el) => items.current.push(el);
-    useEffect(() => {
-        for (let i = 0; i < items.current.length; i++) {
-            items.current[i].addEventListener('click', () => {
-                mobile.current.classList.toggle('toggle');
-            })
-        }})
     const toggle = () => {
         mobile.current.classList.toggle('toggle');
     }
-
     return (
         <header>
             <Typing nome="Luka" />
             <nav>
                 <div className="burgericon">
                     <div className="burgeroptns" ref={mobile}>
-                        <li><a href="#home" ref={pushItems}>HOME</a></li>
-                        <li><a href="#habilidades" ref={pushItems}>HABILIDADES</a></li>
-                        <li><a href="#projetos" ref={pushItems}>PROJETOS</a></li>
-                        <li><a href="#contato" ref={pushItems}>CONTATO</a></li>
+                        <li><a href="#home" onClick={toggle}>HOME</a></li>
+                        <li><a href="#habilidades" onClick={toggle}>HABILIDADES</a></li>
+                        <li><a href="#projetos" onClick={toggle}>PROJETOS</a></li>
+                        <li><a href="#contato" onClick={toggle}>CONTATO</a></li>
                     </div>
                     <input type="checkbox" className="toggler" onClick={toggle} aria-label="Burger" />
                     <div className="hamburger"><div></div></div>
