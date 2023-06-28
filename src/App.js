7a714b307e6d09b0249cf1e19106b0d62c2e163a
Luka Souza <lukasouza77@gmail.com>
2023-06-28 13:56:49 -0300

    .

--- src/App.js
+++ src/App.js
@@ -5,11 +5,26 @@ import Habilidades from './components/habilidades.js';
 import Projetos from './components/projetos.js';
 
 export default function App() {
+
+    window.addEventListener('scroll', () => {animaScroll()});
+    
+    const animaScroll = () => {
+        document.querySelectorAll('.init-hidden').forEach((section) => {
+            const halfSection = window.innerHeight * 0.75;
+            const sectionTop = section.getBoundingClientRect().top;
+            const isSectionVisible = sectionTop - halfSection < 0;
+            if(isSectionVisible) {
+                section.classList.add('init-hidden-off');
+                document.querySelector('.fa-angles-down').classList.add('scroll-hidden');
+            }
+        })
+    }
+
     return (
         <div className="App">
             <Header />
             <Home />
-            <i className="fa-solid fa-angles-down scroll"></i>
+            <i className="fa-solid fa-angles-down"></i>
             <Habilidades />
             <Projetos />
         </div>
