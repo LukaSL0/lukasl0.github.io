dd28c9d25e04b25509141277f84ab998ee13a527
Luka Souza <lukasouza77@gmail.com>
2023-06-28 04:48:47 -0300

    .

--- src/components/modules/projetos-optn.js
+++ src/components/modules/projetos-optn.js
@@ -1,25 +1,5 @@
 export default function ProjetosOptn(props) {
 
-    const imglink = () => {
-        if (props.projeto === "strawberry") {
-            return "https://cdn.discordapp.com/attachments/890745869832454145/1123506243273768960/strawberry.avif";
-        } else if (props.projeto === "eye") {
-            return "https://cdn.discordapp.com/attachments/890745869832454145/1123506242774638683/eye.avif";
-        } else if (props.projeto === "brox") {
-            return "https://cdn.discordapp.com/attachments/890745869832454145/1123505922392727642/brox.avif";
-        }
-    }
-
-    const redirect = () => {
-        if (props.projeto === "strawberry") {
-            return "https://lukasl0.github.io/strawberry/";
-        } else if (props.projeto === "eye") {
-            return "https://lukasl0.github.io/eyeit/";
-        } else if (props.projeto === "brox") {
-            return "https://lukasl0.github.io/brox/";
-        }
-    }
-
     const show = (e) => {
         if (e.target.classList.contains("strawberry") || e.target.classList.contains("eye") || e.target.classList.contains("brox")) {
             document.querySelector(`.txt-${e.target.classList[1]}`).classList.add("animacao2");
@@ -34,8 +14,8 @@ export default function ProjetosOptn(props) {
 
     return (
         <div className="projeto">
-            <img src={imglink()} onMouseOut={(e) => {clear(e)}} onMouseOver={(e) => {show(e)}} onClick={() => {
-                window.open(redirect())
+            <img src={props.imagem} onMouseOut={(e) => {clear(e)}} onMouseOver={(e) => {show(e)}} onClick={() => {
+                window.open(props.redirect)
                 }} alt="" className={"projeto-img " + props.projeto} />
             <p className={"projeto-txt txt-" + props.projeto}>Clique para ser redirecionado</p>
         </div>
--- src/components/projetos.js
+++ src/components/projetos.js
@@ -6,9 +6,18 @@ export default function Projetos() {
         <section className="bloco2 init-hidden">
         <h1 className="h12">PROJETOS</h1>
         <div className="projetos">
-            <ProjetosOptn projeto="strawberry" />
-            <ProjetosOptn projeto="eye" />
-            <ProjetosOptn projeto="brox" />
+            <ProjetosOptn projeto="strawberry"
+                imagem="https://cdn.discordapp.com/attachments/890745869832454145/1123506243273768960/strawberry.avif"
+                redirect="https://lukasl0.github.io/strawberry/"
+            />
+            <ProjetosOptn projeto="eye"
+                imagem="https://cdn.discordapp.com/attachments/890745869832454145/1123506242774638683/eye.avif"
+                redirect="https://lukasl0.github.io/eyeit/"
+            />
+            <ProjetosOptn projeto="brox"
+                imagem="https://cdn.discordapp.com/attachments/890745869832454145/1123505922392727642/brox.avif"
+                redirect="https://lukasl0.github.io/brox/"
+            />
         </div>
         <div className="desc">
             <h2>Todos estes projetos foram feitos para clientes autônomos</h2>
