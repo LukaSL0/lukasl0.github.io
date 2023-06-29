7de22edaae1a6a70be12b0f25007d79ae67e1b8e
Luka Souza <lukasouza77@gmail.com>
2023-06-29 18:21:31 -0300

    '.'

--- src/components/modules/typing.js
+++ src/components/modules/typing.js
@@ -3,14 +3,15 @@ import Typewriter from "typewriter-effect";
 export default function Typing(props) {
     return (
         <div>
-            <p><i className="fa-solid fa-terminal"></i><Typewriter
+            <p><i className="fa-solid fa-terminal"></i></p>
+            <Typewriter
                 onInit={(typewriter) => {
                     typewriter
                         .typeString(props.nome)
                         .pauseFor(250)
                         .start();
                 }}
-            /></p>
+            />
         </div>
     )
 }
