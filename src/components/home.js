0bf47c25415294c7110e86a573bff08b3a90a691
Luka Souza <lukasouza77@gmail.com>
2023-06-30 06:04:17 -0300

    '.'

--- src/components/home.js
+++ src/components/home.js
@@ -6,7 +6,7 @@ export default function Home() {
             <div className="home">
                 <div className="textoprinc">
                     <h1>OLÁ, EU SOU LUKA</h1>
-                    <p>Apaixonado por esportes e tecnoloia, comecei a estudar programação Front-End em 2021. Após concluir diversos cursos comecei a fazer projetos para pessoas autônomas, você pode encontrar alguns destes na seção de projetos. Agora com mais experiência, busco mais projetos para desenvolver e estudo para me tornar um programador fullstack.</p>
+                    <p>Apaixonado por esportes e tecnoloia, comecei a estudar programação Front-End em 2021. Após concluir alguns cursos e estudar por conta própria, comecei a fazer projetos para clientes autônomos, você pode encontrar alguns destes na seção projetos. Agora com mais experiência, busco mais projetos para desenvolver e estudo para me tornar um programador fullstack.</p>
                 </div>
                 <img src="../../assets/al2.avif" alt="" />
             </div>
